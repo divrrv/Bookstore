9a23d887cbee63b3410359a90a48245d9f94a196
divrrv <112497215+divrrv@users.noreply.github.com>
2022-09-20 21:59:34 +0300

    Listpage

--- Bookstore/src/main/java/hh/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/Bookstore/web/BookController.java
@@ -1,16 +1,28 @@
 package hh.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
 	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String getBook(Model model) {
 		return "index";
+	}
+	
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String bookList (Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
 
