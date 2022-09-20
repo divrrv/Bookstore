831f7be635634052531ce506e85508df43e0f4c2
divrrv <112497215+divrrv@users.noreply.github.com>
2022-09-20 20:28:25 +0300

    JPA and Database(new)

--- Bookstore/src/main/java/hh/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/hh/Bookstore/BookstoreApplication.java
@@ -1,13 +1,29 @@
 package hh.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.Bookstore.domain.Book;
+import hh.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book b1 = new Book("Joulu", "Louis D", 1996, "121223", 12.30);
+			Book b2 = new Book("Kesä", "Sini H", 1998, "131321", 12.30);
+			
+			repository.save(b1);
+			repository.save(b2);
+		};
 	}
 
 }
