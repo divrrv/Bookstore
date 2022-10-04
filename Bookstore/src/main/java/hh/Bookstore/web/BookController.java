91226e513300ec3fc3f41216339730acb8023f29
divrrv <112497215+divrrv@users.noreply.github.com>
2022-10-04 22:22:12 +0300

    REST

--- Bookstore/src/main/java/hh/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/Bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package hh.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.Bookstore.domain.Book;
 import hh.Bookstore.domain.BookRepository;
@@ -28,6 +32,18 @@ public class BookController {
 	public String bookList (Model model) {
 		model.addAttribute("books", brepository.findAll());
 		return "booklist";
+	}
+	
+	// RESTful     Get all books
+		@RequestMapping(value = "/books", method = RequestMethod.GET)
+		public @ResponseBody List<Book> bookListRest() {
+			return (List<Book>) brepository.findAll();
+		}
+		
+	// RESTful     Get book by id
+	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return brepository.findById(bookId);
 	}
 	
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
