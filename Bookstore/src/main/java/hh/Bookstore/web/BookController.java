9a43b36654469c3747a8223cf8939b9b2db3375d
divrrv <112497215+divrrv@users.noreply.github.com>
2022-10-12 00:04:56 +0300

    Securing the web application

--- Bookstore/src/main/java/hh/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,11 +23,6 @@ public class BookController {
 	
 	@Autowired
 	private BookRepository brepository;
-	
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String getBook(Model model) {
-		return "index";
-	}
 	
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
 	public String bookList (Model model) {
@@ -60,9 +56,15 @@ public class BookController {
     }    
 	
 	@RequestMapping(value = "/delete{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ADMIN')")
 	public String deleteBook (@PathVariable("id") Long bookId, Model model) {
 		brepository.deleteById(bookId);
 		return "redirect:../booklist";
+	}
+	
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
 	}
 	
 	
